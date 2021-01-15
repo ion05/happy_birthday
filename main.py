9784ef66c528bfbc9e5106a0ed4cc5cdc91c04b7
aayanagarwal05@gmail.com <49789627+aayan05@users.noreply.github.com>
2021-01-15 14:24:13 +0530

    Excel File

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd  # reading our excel file
+import datetime  # for intracting with time and year
+import smtplib  # for a connection between our gmail & python script
+from email.message import EmailMessage  # sending emails
+import os  # intracting with the system
