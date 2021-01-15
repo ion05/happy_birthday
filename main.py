01e3412c80d0de24e7e4fd0bed9e5745684825cc
aayanagarwal05@gmail.com <49789627+aayan05@users.noreply.github.com>
2021-01-15 14:42:07 +0530

    Send email

--- main.py
+++ main.py
@@ -3,3 +3,30 @@ import datetime  # for intracting with time and year
 import smtplib  # for a connection between our gmail & python script
 from email.message import EmailMessage  # sending emails
 import os  # intracting with the system
+from dotenv import load_dotenv
+
+load_dotenv()
+password = os.getenv("PASSWORD")
+
+
+def sendEmail(to, sub, msg):
+    print(f"email to {to} \nsend with subject: {sub} \n message: {msg}")
+    email = EmailMessage()
+    email['from'] = "Aayan Agarwal"
+    email['to'] = f"{to}"
+    email['subject'] = f'{sub}'
+    email.set_content(f"{msg}")
+
+    with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
+
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login("Email", password)
+        smtp.send_message(email)
+        print("Email Send")
+    pass
+
+
+if __name__ == "__main__":
+    df = pd.read_excel("data.xlsx")
+    print(df)
