6fe393897d255ce8376cb9d5bd8b110e0066abf8
aayanagarwal05@gmail.com <49789627+aayan05@users.noreply.github.com>
2021-01-18 14:57:39 +0530

    programme code

--- main.py
+++ main.py
@@ -27,6 +27,21 @@ def sendEmail(to, sub, msg):
     pass
 
 
+# Here if __name__ == “main” block is used to prevent (certain) code from being run when the module is imported in another program.
 if __name__ == "__main__":
     df = pd.read_excel("data.xlsx")
     print(df)
+    today = datetime.datetime.now().strftime("%d-%m")
+    # print(type(today))
+    update = []
+    yearnow = datetime.datetime.now().strftime("%Y")
+    for index, item in df.iterrows():
+        bday = item["Birthday"].strftime("%d-%m")
+        if (bday == today) and yearnow not in str(item["Year"]):
+            sendEmail(item["Email"], 'Happy Birthday' +
+                      item["Name"], item["message"])
+            update.append(index)
+        for i in update:
+            yr = df.loc[i, "Year"]
+            df.loc[i, "Year"] = f"{yr}, {yearnow}"
+        df.to_excel("data.xlsx", index=False)
